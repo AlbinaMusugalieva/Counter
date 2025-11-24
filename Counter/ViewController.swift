74c62556e1d76309ac1be09bd250b643f31a7c0d
Albina <albina_musugalieva@mail.ru>
2025-11-24 13:30:33 +0300

    Sprint 03

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -2,18 +2,73 @@
 //  ViewController.swift
 //  Counter
 //
-//  Created by Альбина on 23.11.2025.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var substractButton: UIButton!
+    @IBOutlet weak var sumButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var changeHistoryTextView: UITextView!
+    
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeHistoryTextView.isScrollEnabled = true
     }
 
-
+    @IBAction func increasingCounter(_ sender: Any) {
+        counter += 1
+        resultLabel.text = "Значение счётчика: \(counter) "
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let formattedDate = dateFormatter.string(from: currentDate)
+        changeHistoryTextView.text += "\n\(formattedDate): значение изменено на +1"
+    }
+    
+    @IBAction func decreasingCounter(_ sender: Any) {
+        if counter == 1 {
+            counter = 0
+            resultLabel.text = "Значение счётчика: \(counter) "
+            let currentDate = Date()
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+            let formattedDate = dateFormatter.string(from: currentDate)
+            changeHistoryTextView.text += "\n\(formattedDate): значение изменено на -1"
+        } else if counter < 1 {
+            counter = 0
+            resultLabel.text = "Значение счётчика: \(counter) "
+            let currentDate = Date()
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+            let formattedDate = dateFormatter.string(from: currentDate)
+            changeHistoryTextView.text += "\n\(formattedDate): попытка уменьшить значение счётчика ниже 0"
+        } else {
+            counter -= 1
+            resultLabel.text = "Значение счётчика: \(counter) "
+            let currentDate = Date()
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+            let formattedDate = dateFormatter.string(from: currentDate)
+            changeHistoryTextView.text += "\n\(formattedDate): значение изменено на -1"
+            
+        }
+    }
+    
+    @IBAction func resetCounter(_ sender: Any) {
+        counter = 0
+        resultLabel.text = "Значение счётчика: \(counter) "
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let formattedDate = dateFormatter.string(from: currentDate)
+        changeHistoryTextView.text += "\n\(formattedDate): значение сброшено"
+    }
+    
+  
 }
 
